0e8c0923275a9cd2e854989dbe9b21a0fcbfdf89
Alex Pogue <apogue@us.ibm.com>
2020-05-05 18:26:59 -0500

    Abstract the fields to append, remove hardcoded ones

--- send_log.py
+++ send_log.py
@@ -1,40 +1,43 @@
 import sys
 import json
+import argparse
 from kafka import KafkaProducer
 
-def printVals(**kwargs):
-    for name, value in kwargs.items():
-        print('{} = {}'.format(name, value))
 
 def main():
-    if len(sys.argv) < 7 or len(sys.argv) > 8:
-        print('Usage: python {} <filename> <cluster> <release> <service_name> <job_id> <topic_name> [<kafka_url:port]'.format(sys.argv[0]))
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='Send a file through Kafka.')
+    parser.add_argument('-f', '--file', help='file to send', metavar='FILE.jtl', required=True)
+    parser.add_argument('-k', '--kafka-url', help='Kafka url and port', metavar='URL:PORT', default='localhost:9092')
+    parser.add_argument('-t', '--kafka-topic', help='Kafka topic to use', metavar='TOPIC_NAME', required=True)
+    parser.add_argument('fields', nargs='*', help='values to append to each csv line before sending', metavar='FIELD=VALUE')
 
-    filename, cluster, release, service, job_id, topic = sys.argv[1:7]
-    kafka_url_and_port = sys.argv[7] if len(sys.argv) > 7 else 'localhost:9092'
+    args = parser.parse_args()
 
-    printVals(filename=filename, cluster=cluster, release=release, service=service, job_id=job_id, topic=topic, kafka_url_and_port=kafka_url_and_port)
+    keys = [arg.split('=')[0] for arg in args.fields]
+    values = [arg.split('=')[1] for arg in args.fields]
 
-    with open(filename) as f:
+    keys_to_append = ','.join(keys)
+    print('keys_to_append = {}'.format(keys_to_append))
+
+    values_to_append = ','.join(values)
+    print('values_to_append = {}'.format(values_to_append))
+
+    with open(args.file) as f:
         content = f.readlines()
+        first_line = ','.join([content[0].strip(), keys_to_append])
+        subsequent_lines = [','.join([line.strip(), values_to_append]) for line in content[1:]]
 
-        to_append = ',{},{},{},{}'.format(cluster, release, service, job_id)
-
-        first_line = ''.join([content[0].strip(), ',clustername,release,servicename,jobid'])
         content_appended = [first_line]
-
-        content_appended += [''.join([line.strip(), to_append]) for line in content[1:]]
-
+        content_appended += subsequent_lines
 
     data = {'lines': content_appended}
 
-    producer = KafkaProducer(bootstrap_servers=[kafka_url_and_port],
+    producer = KafkaProducer(bootstrap_servers=[args.kafka_url],
                              value_serializer=lambda x:
                              json.dumps(x).encode('utf-8'))
 
     print('sending data')
-    producer.send('{}'.format(topic), value=data)
+    producer.send('{}'.format(args.kafka_topic), value=data)
 
 if __name__ == '__main__':
     main()

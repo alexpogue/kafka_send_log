16e5702455ead81db7af7f574d3617fe22e3ec41
Alex Pogue <apogue@us.ibm.com>
2020-05-05 16:57:41 -0500

    Add cluster release

--- send_log.py
+++ send_log.py
@@ -7,21 +7,21 @@ def printVals(**kwargs):
         print('{} = {}'.format(name, value))
 
 def main():
-    if len(sys.argv) < 6 or len(sys.argv) > 7:
-        print('Usage: python {} <filename> <cluster> <service_name> <job_id> <topic_name> [<kafka_url:port]'.format(sys.argv[0]))
+    if len(sys.argv) < 7 or len(sys.argv) > 8:
+        print('Usage: python {} <filename> <cluster> <release> <service_name> <job_id> <topic_name> [<kafka_url:port]'.format(sys.argv[0]))
         sys.exit(1)
 
-    filename, cluster, service, job_id, topic = sys.argv[1:6]
-    kafka_url_and_port = sys.argv[6] if len(sys.argv) > 6 else 'localhost:9092'
+    filename, cluster, release, service, job_id, topic = sys.argv[1:7]
+    kafka_url_and_port = sys.argv[7] if len(sys.argv) > 7 else 'localhost:9092'
 
-    printVals(filename=filename, cluster=cluster, service=service, job_id=job_id, topic=topic, kafka_url_and_port=kafka_url_and_port)
+    printVals(filename=filename, cluster=cluster, release=release, service=service, job_id=job_id, topic=topic, kafka_url_and_port=kafka_url_and_port)
 
     with open(filename) as f:
         content = f.readlines()
 
-        to_append = ',{},{},{}'.format(cluster, service, job_id)
+        to_append = ',{},{},{},{}'.format(cluster, release, service, job_id)
 
-        first_line = ''.join([content[0].strip(), ',clustername,servicename,jobid'])
+        first_line = ''.join([content[0].strip(), ',clustername,release,servicename,jobid'])
         content_appended = [first_line]
 
         content_appended += [''.join([line.strip(), to_append]) for line in content[1:]]
